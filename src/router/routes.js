73a9b7ba659f07bfb475610f0d6c63f3fa08fe19
胡佳能 <hujianeng@hujianengdeMBP.lan>
2018-10-17 22:19:10 +0800

    change

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,17 +1,17 @@
-import Jkwy404 from '../components/jkwy-404.vue';
-import TestHome from '../components/test-home.vue';
+import Page404 from '../components/page-404.vue';
+import Home from '../components/home.vue';
 
 export default [
     {
-        name: 'testHome',
+        name: 'home',
         path: '/',
-        component: TestHome,
+        component: Home,
         meta: { title: '首页' },
     },
     {
         name: '404',
         path: '*',
-        component: Jkwy404,
+        component: Page404,
         meta: { title: '404 当前页面不存在！' },
     }
 ]
